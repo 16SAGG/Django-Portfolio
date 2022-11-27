4db3f452e4e7dfcb4b2053a875b71fc3d82d2f0f
Samuel <sagg160800@gmail.com>
2022-11-27 13:24:54 -0400

    post_view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import post
 
 def posts(request):
-    return render(request, 'posts.html')
+    myPosts = post.objects.all()
+    ctxt = {
+        'myPosts' : myPosts,
+    }
+    return render(request, 'posts.html', ctxt)
+
+def post_detail(request, post_id):
+    myPost = get_object_or_404(post, pk = post_id)
+    ctxt = {
+        'myPost' : myPost,
+    }
+    return render(request, 'post_detail.html', ctxt)
